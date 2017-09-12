807d6320b7e5e3dcdc95af521bc45ef4aba06b02
Leandro Nascimento <fleandro.nascimento@gmail.com>
2017-09-12 17:04:56 -0300

    add bittrex api and http module

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import bittrex from 'api-bittrex';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
